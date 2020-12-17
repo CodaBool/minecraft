44c55d95bbf07bb07ccbbebdda97af96ba41dec3
Coda Bool <CodaBool@pm.me>
2020-12-17 04:21:27 -0500

    new url

--- quote.sh
+++ quote.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# ====== Lambda ======
+# https://g5rkignyck.execute-api.us-east-1.amazonaws.com
+
+# ====== Local ======
+# http://192.168.1.34:5050/quote
+
+
 OUT=$(tail -1 /opt/minecraft/server/logs/latest.log)
 if [[ $OUT == *"joined the game"* ]]; then
-  IP=$(curl https://g5rkignyck.execute-api.us-east-1.amazonaws.com)
+  IP=$(curl http://192.168.1.34:5050/quote)
   /opt/minecraft/tools/mcrcon/mcrcon -p admin "say $IP"
 fi
